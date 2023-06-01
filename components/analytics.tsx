43c11a6ea384deb49caab9637554e48e096ccbe8
Haydn Diniz <56917000+Haydndiniz@users.noreply.github.com>
2023-06-01 20:46:13 +0100

    disaled cv link

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -3,12 +3,14 @@ import { useEffect } from 'react'
 import * as gtag from 'lib/gtag'
 import { useRouter } from 'next/router'
 
+// hotjar ID
 const HJID = 3433831
 const HJSV = 6
 
 
 function MyApp ({ Component, pageProps }) {
   const router = useRouter()
+  // Google analytics init
 useEffect(() => {
     const handleRouteChange = url => {
       gtag.pageview(url)
@@ -18,6 +20,7 @@ useEffect(() => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events])
+  // hotjar init
 useEffect(() => {
     hotjar.initialize(HJID, HJSV)
   }, [])
